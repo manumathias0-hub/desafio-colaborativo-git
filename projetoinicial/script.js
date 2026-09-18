cd89b2eed7e60ca48f9eccd0fcd5d52f030fe2ba
emivick <158385028+Rodrigurs@users.noreply.github.com>
2026-09-18 10:15:01 -0300

    projetoinicial

---
+++ projetoinicial/script.js
@@ -0,0 +1,6 @@
+function mostrarMensagem() {
+    const mensagem = document.getElementById("mensagem");
+
+    mensagem.textContent =
+        "🎉 Parabéns! Você fez seu primeiro commit colaborativo!";
+}
